011f280206d0291c4e608f3c405b30962c1795b8
Itzel Annet <itzelmunozlove@gmail.com>
2025-04-11 19:03:11 -0600

    Se agrego correctamenta la funcion con el modal, y se agrego lo que se vio en clases con lo de ctaegoria

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const main = document.getElementsByTagName("main").item(0);
 const URLMain = "https://fakestoreapi.com/products/";
-const cardsContainer = document.getElementById("cardsContainer");
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+const mainProds = document.getElementById("mainProds");
+const ulMenu = document.getElementById("ulMenu");
 
-function getData(){
-    fetch(URLMain)
+
+function getData(cat){
+    const options = {"method":"GET"};
+    fetch(URLMain+cat, options)
     .then((response) => {
         console.log(response);
         response.json().then((res)=>{
@@ -26,29 +27,76 @@ function getData(){
     })
 }
 
-getData();
+getData("");
+
+
+function getCategories(){
+    const options = {"method":"GET"};
+    fetch(URLMain+"categories/", options)
+    .then((response) => {
+        //console.log(response);
+        response.json().then((res)=>{
+            // console.log(res.length); //20
+            // console.log(res[0].title);
+            //console.log("categories: ", res);
+            res.forEach((cat)=>{
+                ulMenu.insertAdjacentHTML("afterbegin", 
+                `<li><a class="dropdown-item" style="cursor:pointer;" onclick="getData('category/${(cat.replace("'","%27"))}');"> ${cat} </a></li>`);
+            });            
+        });
+        
+    }
+
+    )
+    .catch((err) => {
+        main.insertAdjacentHTML("beforeend", 
+                         `<div class="alert alert-danger" role="alert">
+                             ${err.message}
+                         </div>`);
+    })
+}
+
+getCategories();
+
 
 function createCards(prods) {
-    // crea las cartas vasado en el arreglo de los productos 
-    prods.forEach((products) => {
-        cardsContainer.insertAdjacentHTML("beforeend", 
-            `<div class="col-sm-6 col-md-4 col-lg-3 d-flex">
-            <div class="card" style="width: 18rem;">
-                <img src="${products.image}" class="card-img-top" alt="..." style="width: 100%; height: 250px;">
+    mainProds.innerHTML = "";
+
+    prods.forEach((prod, x) => {
+        const modal = `Modal-${x}`;
+        mainProds.insertAdjacentHTML("beforeend", 
+            `<div class="card" style="width: 18rem;">
+                <img src="${prod.image}" class="card-img-top" alt="${prod.title}" style="height:300px; object-fit:contain;">
                 <div class="card-body">
-                    <h5 class="card-title">${products.title}</h5>
-                    <p class="card-text"> $${products.price}</p>
-                    <p class="card-text">${products.description.slice(0, 100)}...</p>
-                    <button type="button" class="btn btn-primary show-modal-btn"
-                            data-title="${products.title}"
-                            data-desc="${products.description}"
-                            data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                            Ver más
-                    </button>                 
+                    <h5 class="card-title">${prod.title}</h5>
+                    <p class="card-text">${prod.description.slice(0, 100)}</p>
+                    <p class="card-text">$ ${prod.price}</p>
+                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${modal}">
+                        Ver más
+                    </a>
+                    <!-- Modal -->
+                    <div class="modal fade" id="${modal}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="modalTitle-${x}">${prod.title}</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ${prod.description}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                    <button type="button" class="btn btn-primary">Comprar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </div>`);
     });
 }
+
+
 
 
